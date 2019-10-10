6293afe434d064fb306739d8b15232b19648e839
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2019-10-10 23:08:05 +0200

    20191010 - informatyka

--- Klasa 4/informatyka/20191008/main.py
+++ Klasa 4/informatyka/20191008/main.py
@@ -29,6 +29,33 @@ def isSumOfSilnia(number):
     else:
         return False
 
+def nwd(a, b):
+    while b:
+        a, b = b, a%b
+    return a
+
+def findChain(numbers):
+    nwd_value = numbers[0]
+    startingIndex = 1
+    lengthOfChain = 1
+    startOfLongestChain = 0
+    longestLengthOfChain = lengthOfChain
+    nwdOfLongestChain = nwd_value
+    i = 1
+    while (i < len(numbers)):
+        if nwd(nwd_value, numbers[i]) != 1:
+            lengthOfChain += 1
+        else:
+            nwd_value = nwd(numbers[i - 1], numbers[i])
+            startingIndex = i
+            lengthOfChain = 1
+        if lengthOfChain > longestLengthOfChain:
+            startOfLongestChain = startingIndex
+            longestLengthOfChain = lengthOfChain
+            nwdOfLongestChain = nwd_value
+        i += 1
+    return { 'startOfChain': startOfLongestChain, 'firstNumberOfChain': numbers[startOfLongestChain - 1], 'lengthOfChain': longestLengthOfChain + 1, 'nwdOfChain': nwdOfLongestChain }
+
 file = open(os.path.dirname(__file__) + '/Dane/liczby.txt')
 output = open(os.path.dirname(__file__) + '/wyniki.txt', 'a')
 numbers = file.read().splitlines(False)
@@ -42,9 +69,11 @@ for number in numbers:
     if factorize(number):
         counter += 1
 
-output.write('Wynik do zad. 4.1: {}\n'.format(counter))
-output.write('Wynik do zad. 4.2:\n')
-for number in numbers:
-    if isSumOfSilnia(number):
-        output.write('\t{}\n'.format(number))
+# output.write('Wynik do zad. 4.1: {}\n'.format(counter))
+# output.write('Wynik do zad. 4.2:\n')
+# for number in numbers:
+#     if isSumOfSilnia(number):
+#         output.write('\t{}\n'.format(number))
+# output.write('Wynik do zad. 4.3:\n')
+print(findChain(numbers))
 output.close()
