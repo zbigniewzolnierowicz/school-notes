d7778c02e944f78477537b4c7c1777364a861fe7
--global <--global>
2019-10-29 14:05:46 +0100

    20191029 - informatyka

--- Klasa 4/informatyka/20191022/calki.py
+++ Klasa 4/informatyka/20191022/calki.py
@@ -1,6 +1,19 @@
 def function(x):
-    return x*x*x + 4(x*x) + 5 * x - 7
+    return x**2 - x - 3
 
-def calculateField(func, p, q, n = 1):
-    h = (q-p)/n
-    
+sumOfFields = 0
+i = 1
+p = int(input('Początek funkcji: '))
+q = int(input('Koniec funkcji: '))
+n = 0
+temp = input('Dokładność funkcji: ')
+if temp == '':
+    n = 1
+else:
+    n = int(temp)
+dl = (q - p) / n
+while (i < n):
+    sumOfFields += abs(function(p+i*dl))
+    i += 1
+totalField = dl/2*(abs(function(p))+abs(function(q))+2*sumOfFields)
+print(totalField)
